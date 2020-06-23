223ee0c6353a56abfd623440981e2bfa0c43939b
AmmarKhan <khanammar9836@gmail.com>
2020-06-24 00:00:57 +0500

    Assign 6 JS ch21-38

---
+++ app.js
@@ -0,0 +1,110 @@
+//                                  chapter 21-25
+// task 1
+// var fname = prompt("Enter your first name: ");
+// var lname = prompt("Enter your last name: ");
+// var fullName = "Your full name is " + fname+lname;
+// alert(fullName)
+
+// task 2
+// var a = prompt("What is your favorite mobile?")
+// var b = a.length;
+// console.log("My favourite phone is: " +a);
+// console.log("Length of string is: " +b);
+
+// task 3
+// var a = "Pakistani";
+// console.log("String : "+a);
+// console.log("Index of 'n' is: "+a.indexOf('n'));
+
+// task 4
+// var a = "Hello World";
+// console.log("String : "+a);
+// console.log("The last index of letter 'l' is: "+a.lastIndexOf('l'));
+
+// task 5
+// var a = ["Pakistani"];  
+// console.log(a.indexOf([3]));
+
+// task 6
+// var fname = prompt("Enter your first name: ");
+// var lname = prompt("Enter your last name: ");
+// var fullName = "Your full name is " + fname.concat(lname);
+// alert(fullName)
+
+// task 7
+// var a = "Hyderabad";
+// var b = a.replace("Hyder", "Islam");
+// console.log('City: '+a);
+// console.log('After replacement: '+b);
+
+// task 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var b = message.replace(/and/g, "&");
+// console.log(b);
+
+// task 9
+// var a ="472";
+// console.log(a+ " is: "+typeof(a));
+// var b =+"472";
+// console.log(b+ " is: "+typeof(b));
+
+// task 10
+// var a = prompt("Enter string : ");
+// var b = a.toUpperCase();
+// console.log(b);
+
+// task 11
+// var a = prompt("Enter string : ");
+// var b = a.replace(/\b[a-z]/g, (x) => x.toUpperCase());
+// console.log(b);
+
+// task 12
+// var num = "3536";
+// console.log(typeof(num));
+
+// task 13
+// var a = prompt("Enter your name : ");
+// if(a==String.fromCharCode(33,44,46,64)){
+//     prompt("Invalid username.");
+// }
+// else{
+//     prompt("Valid Username");
+// }
+
+// task 14      
+// var b = prompt("Enter any eatable's name: ");
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// if (b===a[0] || b===a[1] || b===a[2] || b===a[3] || b===a[4])
+// {
+//     prompt("Item found.")
+// }
+// else{
+//     prompt("Item not found.")
+// }
+
+// task 15
+// var pass = prompt("Please enter password: ");
+// if(pass = String.fromCharCode(0-9) && String.fromCharCode(65-90) && String.fromCharCode(97-122))
+// {
+//    alert("Password is valid")
+// }
+// else if(pass.charAt[0] = String.fromCharCode(0-9))
+// {
+//    alert("Password is not valid")
+// }
+// else if(pass.length <= 6)
+// {
+//    alert("Password is valid")
+// }
+// else{
+//     alert("Invalid password")
+// }
+
+// task 16
+// var university = "University of Karachi";
+// var b = university.split("");
+// console.log(b);
+
+// task 17
+var a = prompt("Enter anything: ");
